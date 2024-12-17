d48f0b0ae639750737da013497f408a33c8b8347
Ben Lai <laichunpongben@gmail.com>
2024-12-17 15:09:19 +0800

    fix: clean up imports

--- src/chat/knowledge.py
+++ src/chat/knowledge.py
@@ -1,13 +1,10 @@
 # knowledge.py
 
-from typing import List, Any
 from phi.vectordb.pgvector import PgVector
-from config import POSTGRES_CONNECTION
 from utils.llm_helper import get_embedder
-from utils.url_helper import is_valid_url, normalize_url
-from phi.utils.log import logger
 
 from .custom_knowledge_base import CustomKnowledgeBase
+from config import POSTGRES_CONNECTION
 
 # Initialize a unified PgVector for all documents
 vector_db = PgVector(
